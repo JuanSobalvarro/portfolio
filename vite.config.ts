d252776df4a11a7060b835d9d0b112d660019d9f
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2026-03-22 03:12:23 -0600

    fix: multi target deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,24 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  const isGitHubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
+  return {
+    base: isGitHubPages ? '/portfolio/' : '/',
+    
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      }
+    },
+    build: {
+      outDir: 'dist',
+      assetsInlineLimit: 0,
     }
   }
 })
